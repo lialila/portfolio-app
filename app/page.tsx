62467aff833ff5ef743a7a7d774c5a83e60c2dff
Uliana Cheklina <uche0790@gmail.com>
2023-05-21 22:48:49 +0200

    Add vertical navbar, add sections, add navigation links, add styling for navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,71 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+// import Image from 'next/image';
+import styles from './page.module.scss';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <h1 id="About">Intro</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim quibusdam
+        dolorum cum repellat doloribus sunt minima, recusandae velit in
+        assumenda modi, dolorem molestiae est possimus dolores molestias
+        repudiandae cumque asperiores? Lorem ipsum dolor sit amet consectetur,
+        adipisicing elit. A odit nulla voluptatem dicta pariatur atque inventore
+        natus animi ab, expedita labore quo commodi delectus dolore nisi,
+        dolorum vero sed architecto. Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Enim quibusdam dolorum cum repellat doloribus sunt
+        minima, recusandae velit in assumenda modi, dolorem molestiae est
+        possimus dolores molestias repudiandae cumque asperiores? Lorem ipsum
+        dolor sit amet consectetur, adipisicing elit. A odit nulla voluptatem
+        dicta pariatur atque inventore natus animi ab, expedita labore quo
+        commodi delectus dolore nisi, dolorum vero sed architecto. Lorem, ipsum
+        dolor sit amet consectetur adipisicing elit. Enim quibusdam dolorum cum
+        repellat doloribus sunt minima, recusandae velit in assumenda modi,
+        dolorem molestiae est possimus dolores molestias repudiandae cumque
+        asperiores? Lorem ipsum dolor sit amet consectetur, adipisicing elit. A
+        odit nulla voluptatem dicta pariatur atque inventore natus animi ab,
+        expedita labore quo commodi delectus dolore nisi, dolorum vero sed
+        architecto.
+      </p>
+      <h1 id="Projects">Projects</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim quibusdam
+        dolorum cum repellat doloribus sunt minima, recusandae velit in
+        assumenda modi, dolorem molestiae est possimus dolores molestias
+        repudiandae cumque asperiores? Lorem ipsum dolor sit amet consectetur,
+        adipisicing elit. A odit nulla voluptatem dicta pariatur atque inventore
+        natus animi ab, expedita labore quo commodi delectus dolore nisi,
+        dolorum vero sed architecto.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim quibusdam
+        dolorum cum repellat doloribus sunt minima, recusandae velit in
+        assumenda modi, dolorem molestiae est possimus dolores molestias
+        repudiandae cumque asperiores? Lorem ipsum dolor sit amet consectetur,
+        adipisicing elit. A odit nulla voluptatem dicta pariatur atque inventore
+        natus animi ab, expedita labore quo commodi delectus dolore nisi,
+        dolorum vero sed architecto.
+      </p>
+      <h1 id="Skills">Skills</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim quibusdam
+        dolorum cum repellat doloribus sunt minima, recusandae velit in
+        assumenda modi, dolorem molestiae est possimus dolores molestias
+        repudiandae cumque asperiores? Lorem ipsum dolor sit amet consectetur,
+        adipisicing elit. A odit nulla voluptatem dicta pariatur atque inventore
+        natus animi ab, expedita labore quo commodi delectus dolore nisi,
+        dolorum vero sed architecto.
+      </p>
+      <h1 id="Contact">Contact</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim quibusdam
+        dolorum cum repellat doloribus sunt minima, recusandae velit in
+        assumenda modi, dolorem molestiae est possimus dolores molestias
+        repudiandae cumque asperiores? Lorem ipsum dolor sit amet consectetur,
+        adipisicing elit. A odit nulla voluptatem dicta pariatur atque inventore
+        natus animi ab, expedita labore quo commodi delectus dolore nisi,
+        dolorum vero sed architecto.
+      </p>
     </main>
-  )
+  );
 }
